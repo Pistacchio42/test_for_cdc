a4cb323d82644b4c5ed11f25947bd3fb400a8842
Stefano <stefano.lamorgia@starcode.it>
2024-07-17 17:12:18 +0200

    funzionante ma c'è un carattere che non va

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:usb_serial/transaction.dart';
 import 'package:usb_serial/usb_serial.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -16,12 +15,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'CDC template',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Test di utilizzo del dispositivo CDC'),
     );
   }
 }
@@ -36,20 +36,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String update="";
-  String debuggo="debugo";
+
+    String update="";
+    List<Widget> serialData=[];
+    //controlla che non siano nulli; va messa fuori per fare la chiusura al termine della app
+    UsbPort? port;
 
   Future<void> read() async {
+    //lista tutti i devices che ci sono
     List<UsbDevice> devices = await UsbSerial.listDevices();
-    print(devices);
 
+    //controlla che nn siano nulli
     UsbPort? port;
     if (devices.length == 0) {
       return;
     }
+
+    //crea una porta; se la lettura va fatta da più dispositivi com questa $port deve essere una lista
     port = await devices[0].create();
 
+    //apri la/e porta/e
     bool openResult = await port!.open();
     if ( !openResult ) {
       print("Failed to open");
@@ -59,23 +65,34 @@ class _MyHomePageState extends State<MyHomePage> {
     await port.setDTR(true);
     await port.setRTS(true);
 
+    //questi sono i parametri di lettura delle porte COM/CDM, si può cambiare qui o direttamente dal dispositivo
     port.setPortParameters(115200, UsbPort.DATABITS_8,
         UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
 
-    // print first result and close port.
+    await port.write(Uint8List.fromList([0x01, 0x54, 0x04]));
+
+    //leggi dalla porta e fai una traduzione da Uint8List a Stringa in questo caso
     port.inputStream!.listen((Uint8List event) {
       print(event);
       setState(() {
-        String s = new String.fromCharCodes(event);
-        var outputAsUint8List = new Uint8List.fromList(s.codeUnits);
+        String s = new String.fromCharCodes(event); /// <-molto importante
         update="$s";
+        serialData.add(Text(s));
       });
-      //port!.close();
     });
 
     //01 54 04 apertura
     // 01 50 04 chiusura
+    //questi numeri sono esadecimali per istruzioni al dispositivo com; riferite al dispositivo 0x1EAB/0x1D06: NewLand;
   }
+
+
+    @override
+    void dispose() {
+      super.dispose();
+    //chiusura della porta
+      port!.close();
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -88,11 +105,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(update),
-            Text(debuggo),
+            ...serialData,
           ],
         ),
       ),
